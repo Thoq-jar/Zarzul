5e2f1cbd18813f54268576c2f97dda645cc059b0
Thoq-jar <125930852+Thoq-jar@users.noreply.github.com>
2023-02-20 18:01:13 -0500

    Add files via upload

---
+++ Zarzul.B1.Release/LiquidBounce++/scripts/NoFall.js
@@ -0,0 +1,48 @@
+﻿var scriptName = "Nofall";
+var scriptVersion = 1.0;
+var scriptAuthor = "Minger";
+var NofallModule = moduleManager.getModule("NOFALL");
+
+var Nofall= new Nofall();
+
+var client;
+
+function Nofall() {
+    this.getName = function() {
+        return "Nofall";
+    };
+
+    this.getDescription = function() {
+        return "";
+    };
+
+    this.getCategory = function() {
+        return "Fun";
+    };
+    this.onEnable = function() {
+		NofallModule.setState(false)
+    }
+    this.onUpdate = function() {
+		mc.thePlayer.setSprinting(true)
+        if (mc.gameSettings.keyBindJump.isKeyDown() && (mc.thePlayer.fallDistance > 5)) {
+
+			NofallModule.setState(true)
+
+}else{
+		NofallModule.setState(false)
+		}
+}
+    this.onDisable = function () {
+        NofallModule.setState(true)
+    }
+}
+
+function onLoad() {}
+
+function onEnable() {
+    client = moduleManager.registerModule(NofallPlus);
+}
+
+function onDisable() {
+    moduleManager.unregisterModule(client);
+}
